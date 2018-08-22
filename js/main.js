1bd69d09556815dd39f2993b37cb58c455010a01
csslick <digitone@naver.com>
2018-08-22 16:42:49 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
 $(function(){
+		
+	// 능력치 그래프 
+	var about_top = $('#skill').offset().top;
+
+	$(window).on('scroll', function(){
+		var win_scroll_top = $(window).scrollTop();
+		var point = [90, 70, 80, 70, 50, 60];
+		
+		// #skill 섹션에 도달하면 그래프 애니메이션 시작
+		if(win_scroll_top >= about_top){
+			for(var i = 0; i < 6; i++){
+				$('#skill .skill_list span')
+					.eq(i)
+					.animate({width: point[i]+'%'});
+			}	
+		}	
+	});
+	
+	
 	// 비쥬얼 영역에 마우스가 올라가면 애니메이션 재생
 	$('#visual').on('mouseover', function(){
-		$('.visual_txt')
+		$('.visual_txt ')
 			.css('animation-play-state', 'running');
 	});
 	
